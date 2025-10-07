ed83d4c6315ec4b367f8f698fb5c1f8a349fdcbf
Alvis97 <101591973+Alvis97@users.noreply.github.com>
2025-10-07 10:59:19 +0100

    Animating heart
    Add animation for heart in homepage

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import homeStyle from "../styles/home.module.scss";
 import LogoLarge from './LogoLarge';
+import LargeHeart from "../assets/LargeHeart.svg";
+import LargeHeart6 from "../assets/LargeHeart6.svg";
+import HeartShadow from './HeartShadow';
 
 interface HomeSectionProps {
     isDarkmodeSelected: boolean;
@@ -10,25 +13,31 @@ const HomeSection: React.FC<HomeSectionProps> = ({ isDarkmodeSelected }) => {
   return (
     <section className={`${homeStyle.homeSection} ${isDarkmodeSelected ? homeStyle.dark : ''}`}>
         <div   className={homeStyle.circle}></div>
+
         <div  className={homeStyle.textContent}>
-            <span   className={homeStyle.name}>Alva Jonsson</span>
-            <span  className={homeStyle.topic}>WEB & GRAPHICS</span>
-            <div  className={homeStyle.category}>
-                <span>Web2</span>
-                <span>Graphics</span>
-                <span>Web3</span>
+            <div className={homeStyle.heading}>
+            <h1>Web-developer / graphics</h1>
             </div>
-            <div  className={homeStyle.hearts}>
+            <div  className={homeStyle.description}>
+              <p>Curious at heart. I design, develop and dive deep into creative technologies - from web2 layouts to web3 experiments. <br>
+              </br> 
+              <span>/* Just exploring what the web can be */</span>
+              </p>
+            </div>
+        </div>
+
+        <div  className={homeStyle.hearts}>
             <LogoLarge className={`${homeStyle.heart1} ${homeStyle.heart}`} />
             <LogoLarge className={`${homeStyle.heart2} ${homeStyle.heart}`} />
             <LogoLarge className={`${homeStyle.heart3} ${homeStyle.heart}`} />
-            <LogoLarge className={`${homeStyle.heart1} ${homeStyle.heart}`} />
-            </div>
-            <div  className={homeStyle.cities}>
-                <span className={homeStyle.town}>London</span>
-                <span className={homeStyle.town}>Örebro</span>
-            </div>
-
+            <LogoLarge className={`${homeStyle.heart4} ${homeStyle.heart}`} />
+        </div>
+         
+        <div  className={homeStyle.heartShadows}>
+            <HeartShadow className={`${homeStyle.shadow1} ${homeStyle.shadow}`} />
+            <HeartShadow className={`${homeStyle.shadow2} ${homeStyle.shadow}`} />
+            <HeartShadow className={`${homeStyle.shadow3} ${homeStyle.shadow}`} />
+            <HeartShadow className={`${homeStyle.shadow4} ${homeStyle.shadow}`} />
         </div>
       
     </section>
