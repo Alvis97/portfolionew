e8ba67a68822efd10b03d83a49577f000b185d2b
Alvis97 <101591973+Alvis97@users.noreply.github.com>
2026-01-20 10:21:32 +0000

    Update web3 project container

--- src/components/Web3.tsx
+++ src/components/Web3.tsx
@@ -49,66 +49,67 @@ function Web3Content( { setModalContent }: Web3Props) {
     }
   }, [publicKey]);
 
-  if (!publicKey) {
-    return (
-      <div className={style.lockContainer}>
-      <h2>To access my Web3 projects, please connect your crypto wallet.</h2>
-      <p>(This is free — you won’t be charged anything.)</p>
-    
-    <WalletMultiButton/>
-
-    <p className={style.noWallet}>
-    No wallet? Have a peek <button>here!</button>
-  </p>
-    </div>
-    );
-  }
-
   return (
-    <div className={style.loggedInContent}>
-      <div className={style.buttonContainer}>
-        <WalletMultiButton /> 
-      </div>
-    <div className={style.web3CardsContainer}>  
-    {web3ModalContent.map((item: Web3PostItem, index: number) => (
+    <div className={style.web3Wrapper}>
 
-      <WebCard
-      key={index}
-      img={item.imgMain}
-      title={item.title}
-      desc={item.desc}
-      tool={item.tool}
-      onClick={() => setModalContent(
-        <>
-              <div className={style.moduleContainer}>
-              <div className={style.textContainer}>
-              <h1>{item.title}</h1>
-              <div className={style.roleContainer}>
-              <p>{item.role}</p>
-              <p>{item.tool}</p>
-              </div>
-              <p>{item.text1}</p>
-              <h2>{item.heading2}</h2>
-              <p>{item.text2}</p>
-              {item.img2 && <img src={item.img2} alt="" />}
-              <h2>{item.heading3}</h2>
-              <p>{item.text3}</p>
-              {item.img3 && <img src={item.img3} alt="" />}
-              <h2>{item.heading4}</h2>
-              <p>{item.text4}</p>
-              {item.img4 && <img src={item.img4} alt="" />}
-              <p>{item.lastWord}</p>
-              </div>
-              <div className={style.imgContainer}>
-              <img className={style.image} src={item.img} alt="" />
-              </div>
-              </div>
-        </>
-      )
-    }
-    />
-    ))}
-    </div>
+      <div 
+      className={`${style.web3CardsContainer} ${
+        !publicKey ? style.locked : ''
+      }`}
+      >  
+            {web3ModalContent.map((item: Web3PostItem, index: number) => (
+
+              <WebCard
+              key={index}
+              img={item.imgMain}
+              title={item.title}
+              desc={item.desc}
+              tool={item.tool}
+              onClick={() => setModalContent(
+                <>
+                      <div className={style.moduleContainer}>
+                      <div className={style.textContainer}>
+                      <h1>{item.title}</h1>
+                      <div className={style.roleContainer}>
+                      <p>{item.role}</p>
+                      <p>{item.tool}</p>
+                      </div>
+                      <p>{item.text1}</p>
+                      <h2>{item.heading2}</h2>
+                      <p>{item.text2}</p>
+                      {item.img2 && <img src={item.img2} alt="" />}
+                      <h2>{item.heading3}</h2>
+                      <p>{item.text3}</p>
+                      {item.img3 && <img src={item.img3} alt="" />}
+                      <h2>{item.heading4}</h2>
+                      <p>{item.text4}</p>
+                      {item.img4 && <img src={item.img4} alt="" />}
+                      <p>{item.lastWord}</p>
+                      </div>
+                      <div className={style.imgContainer}>
+                      <img className={style.image} src={item.img} alt="" />
+                      </div>
+                      </div>
+                </>
+              )
+            }
+            />
+            ))}
+      </div>
+          
+            {!publicKey && (
+            <div className={style.lockContainer}>
+            <h2>Connect your wallet to access my Web3 projects.</h2>
+            <p>(This is free — no charges will be made.)</p>
+
+            <WalletMultiButton/>
+
+            <p className={style.noWallet}>
+            Don’t have a wallet? <br></br>
+            You can still preview the projects <button>here!</button>
+            </p>
+            </div>
+      )}        
   </div> 
   )
 }
