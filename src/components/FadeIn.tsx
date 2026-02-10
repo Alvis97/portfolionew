88ff8a4fe33749c303104ad3d889e4cb572eae48
Alvis97 <101591973+Alvis97@users.noreply.github.com>
2026-02-10 11:07:02 +0000

    Remove unessesery code

--- src/components/FadeIn.tsx
+++ src/components/FadeIn.tsx
@@ -11,7 +11,7 @@ const FadeUp = ({ children, delay = 0 }: FadeUpProps) => (
     initial={{ opacity: 0, y: 20 }}
     whileInView={{ opacity: 1, y: 0 }}
     viewport={{ once: true, amount: 0.3 }}
-    transition={{ duration: 1.6, ease: "easeOut", delay }}
+    transition={{ duration: 1, ease: "easeOut", delay }}
   >
     {children}
   </motion.div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -85,7 +85,7 @@ function Projects() {
         
      
             )}
-                 </FadeUp>
+           </FadeUp>
           </div>
        
 
